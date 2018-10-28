8e85ccae10f348c7adcd0d5f11ee86c42bef16fb
KenshuHori <kenshuhori@gmail.com>
2018-10-28 16:49:47 +0900

    rename file of properties
    use properties.json for retrieve channelSecret and channelAccessToken

--- server.js
+++ server.js
@@ -4,8 +4,8 @@ const express = require('express');
 const line = require('@line/bot-sdk');
 const PORT = process.env.PORT || 3000;
 const config = {
-    channelSecret: '6c47315ec1f0709ce2aaad0cd29fa480',
-    channelAccessToken: 'eKLxOeZmVZdQc108lLhrgN0PDazS+iF7l40uB2n+G8vHrfU4aT8W/QDwyl6+XL3+jQTlLqZOe0Kf6ngk33DVT8t9y+TPFEvVtzfsdSvqmd15CjHWPA6Ln8Zk0XFm5BPGa/fITzQLXWdZhfRa7uPS1QdB04t89/1O/w1cDnyilFU='
+    channelSecret: '',
+    channelAccessToken: ''
 };
 const app = express();
 
@@ -15,16 +15,14 @@ function init () {
 
 function initProperties() {
     var fs = require("fs");
-    fs.readFile("linebot.properties", "utf8", (error, data) => {
-        if (error) {
-            console.log('read properites occures error');
-            return;
-        }
-        console.log(data);
-        console.log(data.Channel_Secret);
-        console.log(data.Channel_Access_Token);
-    })
+    var json = JSON.parse(fs.readFileSync('properties.json', 'utf8'));
+    console.log(json.channelSecret);
+    console.log(json.channelAccessToken);
+    config.channelSecret = json.channelSecret;
+    config.channelAccessToken = json.channelAccessToken;
 }
+
+init();
 
 app.post('/webhook', line.middleware(config), (req, res) => {
     console.log(req.body.events);
@@ -45,6 +43,5 @@ function handleEvent(event) {
     });
 }
 
-init();
 app.listen(PORT);
 console.log(`Server running at ${PORT}`);
